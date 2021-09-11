29b060ae40bfcdf8843ccbb9f6163d328b46dee4
Foysal Ahmed <foysal.tech@staffasia.org>
2021-09-11 15:37:25 +0600

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+import { Grid } from '@material-ui/core'
+
+export default function Home() {
+    return (
+        <div>
+            <Grid container justifyContent='center'>
+                <h1>Home</h1>
+            </Grid>
+        </div>
+    )
+}
