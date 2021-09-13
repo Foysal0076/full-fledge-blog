e25454af03996ccad5963b31112fdfb6a66ce70c
Foysal Ahmed <foysal.tech@staffasia.org>
2021-09-13 21:52:21 +0600

    Article Card

--- src/components/Home/FeaturedArticles.js
+++ src/components/Home/FeaturedArticles.js
@@ -1,13 +1,27 @@
-import { Container, Grid } from '@material-ui/core'
 import React from 'react'
+import { Box, Container, Grid } from '@material-ui/core'
+import styles from './Home.module.css'
+import ArticleCard from '../Shared/Cards/ArticleCard/ArticleCard'
 
 const FeaturedArticles = () => {
     return (
         <Container>
             <Grid container justifyContent='center'>
-                <h2>
-                    Featured Articles
-                </h2>
+                <h2 className={styles.title}>Featured Articles</h2>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <ArticleCard />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <ArticleCard />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <ArticleCard />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <ArticleCard />
+                    </Grid>
+                </Grid>
             </Grid>
         </Container>
     )
